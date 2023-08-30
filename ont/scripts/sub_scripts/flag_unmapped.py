e7876dada45ec2f722d205331ef5696154c5deaf
AnthonyPiot91 <anthony.piot.1@ulaval.ca>
2023-08-30 15:00:56 -0400

    flag_unmappe.py

--- ont/scripts/sub_scripts/flag_unmapped.py
+++ ont/scripts/sub_scripts/flag_unmapped.py
@@ -7,14 +7,17 @@ Usage: python flag_unmapped.py ref.fasta.fai input.sam output.sam
 import re
 import sys
 
-# Minimal number of aligned bases required to keep read
-MATCH = 1000
+# Minimum mapping quality
+MIN_MAPQUAL = 30
+# Minimum number of aligned bases required to keep read
+MIN_MATCH = 1000
 # Maximum number of clipped bases at a read extremity and within the reference
-MAX_CLIP_WITHIN = 200
+MAX_CLIP_WITHIN = 100
 # Maximum number of clipped bases at a read extremity and within the reference
 # for reads going past the reference
 MAX_CLIP_WITHIN_EDGE = 200
 
+# Define input and output file
 REF_FILE = open(sys.argv[1], "r")
 SAM_FILE = open(sys.argv[2], "r")
 OUT_SAM = open(sys.argv[3], "w")
@@ -28,36 +31,38 @@ for line_ref in REF_FILE:
     length = col_ref[1]
     dict[seq_ids] = length
 
-# Go through each line in input SAM file
+# Go through each line in the input SAM file
 for line in SAM_FILE:
 
     # Write headers to SAM output
     if line.startswith("@"):
         OUT_SAM.write(line)
 
-    # Go through each aligned reads in the SAM input
+    # Go through each read in the SAM input
     else:
         col = line.split("\t")
 
-        # If unmapped read write to output and continue to next read
-        if col[1] == "4":
+        # Store read attributes
+        flag = col[1]
+        id = col[2]
+        read_start = int(col[3])
+        mapqual = col[4]
+        CIGAR_str = col[5]
+
+	# If unmapped read write to output and continue to next read
+        if flag == "4":
             OUT_SAM.write(line)
             continue
 
         # Remove secondary alignment
-        elif col[1] == "256" or col[1] == "272":
+        elif flag == "256" or flag == "272":
             continue
 
         # Remove supplementary alignment
-        elif col[1] == "2048" or col[1] == "2064":
+        elif flag == "2048" or flag == "2064":
             continue
 
-        # Store read attributes
-        id = col[2]
-        read_start = int(col[3])
-        CIGAR_str = col[5]
-
-        # Store reference sequence attribute
+        # Store length of the reference sequence
         ref_end = int(dict[id])
 
         # Calculate the number of aligned bases
@@ -66,8 +71,8 @@ for line in SAM_FILE:
         match = [int(i) for i in match]
         sum_match = sum(match)
 
-        # Reflag if the number of aligned bases is less than MATCH
-        if sum_match < MATCH:
+        # Reflag read if mapping quality < to MIN_MAPQUAL
+        if int(mapqual) < MIN_MAPQUAL:
             col[1] = "4"
             col[2] = "*"
             col[3] = "0"
@@ -75,7 +80,16 @@ for line in SAM_FILE:
             col[5] = "*"
             line = "\t".join(col)
 
-        # Reflag if a stretch of more than 99 insertions or deletions inside the read
+        # Reflag read if the number of aligned bases is less than MIN_MATCH
+        elif sum_match < MIN_MATCH:
+            col[1] = "4"
+            col[2] = "*"
+            col[3] = "0"
+            col[4] = "0"
+            col[5] = "*"
+            line = "\t".join(col)
+
+        # Reflag read if a stretch of more than 99 insertions or deletions inside the read
         elif re.search("[A-Z][0-9]{3,}[D,I][0-9]", CIGAR_str):
             col[1] = "4"
             col[2] = "*"
@@ -84,7 +98,7 @@ for line in SAM_FILE:
             col[5] = "*"
             line = "\t".join(col)
 
-        # Reflag if a strech of more than 99 clipped bases inside the read
+        # Reflag read if a strech of more than 99 clipped bases inside the read
         elif re.search("[A-Z][0-9]{3,}[H,S][0-9]", CIGAR_str):
             col[1] = "4"
             col[2] = "*"
@@ -102,7 +116,7 @@ for line in SAM_FILE:
                 clipped_bases_start = clipped_bases_start.replace("H","").replace("S","")
                 clipped_bases_start = int(clipped_bases_start)
 
-                # Reflag if clipped bases are within the reference sequence
+                # Reflag read if clipped bases are within the reference sequence
                 # and greater than MAX_CLIP_WITHIN
                 if clipped_bases_start < read_start and clipped_bases_start > MAX_CLIP_WITHIN:
                     col[1] = "4"
@@ -112,7 +126,7 @@ for line in SAM_FILE:
                     col[5] = "*"
                     line = "\t".join(col)
 
-                # Reflag if clipped bases reach the start of the reference and more than
+                # Reflag read if clipped bases reach the start of the reference and more than
                 # MAX_CLIP_WITHIN_EDGE clipped bases are within the reference sequence
                 elif clipped_bases_start > read_start and read_start > MAX_CLIP_WITHIN_EDGE:
                     col[1] = "4"
@@ -137,7 +151,7 @@ for line in SAM_FILE:
                 aligned_read_end = read_start + sum_match3
                 clipped_end_pos = aligned_read_end + clipped_bases_end
 
-                # Reflag if clipped bases are within the reference sequence and greater
+                # Reflag read if clipped bases are within the reference sequence and greater
                 # than MAX_CLIP_WITHIN
                 if clipped_end_pos < ref_end and clipped_bases_end > MAX_CLIP_WITHIN:
                     col[1] = "4"
@@ -147,7 +161,7 @@ for line in SAM_FILE:
                     col[5] = "*"
                     line = "\t".join(col)
 
-                # Reflag if clipped bases reach the end of the reference and more than
+                # Reflag read if clipped bases reach the end of the reference and more than
                 # MAX_CLIP_WITHIN_EDGE clipped bases are within the reference sequence
                 else:
                     clipped_end_within = ref_end - aligned_read_end
